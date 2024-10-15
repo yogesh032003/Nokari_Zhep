877ba7de74f8c8e2fa9c4401991d357cec8e7863
yogesh032003 <yogeshsn03@gmail.com>
2024-10-15 23:14:59 +0530

    changes saved

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,16 @@
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default AppLayout;
